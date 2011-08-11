8a2bcca9a6c742539725b6ef5dcc90283f2625f4
Diogo Resende <dresende@thinkdigital.pt>
2011-08-11 13:16:48 +0100

    added some comments; improved error handling

--- examples/simple.js
+++ examples/simple.js
@@ -4,6 +4,10 @@ var b = new Butler();
 b.add(asyncAdd, 1, 2)
  .add(asyncDouble)
  .wait(function (err, values, butler) {
+ 	if (err) {
+ 		console.log("error!", err);
+ 		return [ 10 ];
+ 	}
  	console.log("%d - 1 = %d", values[0], values[0] - 1);
  	butler.set(values[0] - 1);
 
@@ -12,6 +16,14 @@ b.add(asyncAdd, 1, 2)
  		butler.resume();
  	}, 2e3);
  	return false;
+ })
+ .add(asyncDouble)
+ .wait(function (err, values, butler) {
+ 	if (err) {
+ 		return console.log("error!", err);
+ 	}
+ 	console.log("%d + 1 = %d", values[0], values[0] + 1);
+ 	return [ values[0] + 1 ];
  })
  .add(asyncDouble)
  .wait(function (err, values, butler) {
@@ -26,8 +38,18 @@ function asyncAdd(a, b, cb) {
 };
 
 function asyncDouble(v, cb) {
-	setTimeout(function () {
-		console.log("%d * 2 = %d", v, v * 2);
-		cb(null, v * 2);
-	}, 1e3);
+	// this is used to check if `this.` works
+	this.doubleIt = function (v) { return v * 2; };
+
+	setTimeout((function (that) {
+		return function () {
+			var result = that.doubleIt(v);
+
+			console.log("%d * 2 = %d", v, result);
+			if (result < 10) {
+				return cb({ message: "Double value below 10" });
+			}
+			cb(null, result);
+		};
+	})(this), 1e3);
 };
